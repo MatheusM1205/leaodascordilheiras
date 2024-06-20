e9c3a34d1bd4ee9bbd2b90357371e6d44bc2a1e2
Matheus Medeiros <matheus.henrique.medeiros.ribeiro@outlook.com.br>
2024-06-20 14:24:24 -0300

    ...

--- app/ui/cabecalho.js
+++ app/ui/cabecalho.js
@@ -13,7 +13,7 @@ export default function Cabecalho(){
                     <a className="*:inline *:hover:fill-purple-500" href="https://www.instagram.com/maracatu.leao.das.cordilheiras/" target="_blank"><FaInstagram color="gray"/></a>
                     <a className="*:inline *:hover:fill-red-500" href="https://www.youtube.com/@LeaodasCordilheiras" target="_blank"><FaYoutube color="gray"/></a>
                 </div>
-                <div className="basis-1/3"></div>
+                <div className="basis-1/3">Teste</div>
                 <div className="space-x-4 basis-1/3 text-right fullhd:text-center">
                     <a className="*:inline *:hover:fill-slate-100" href="tel:84991095932" target="_blank"><BsTelephone color="gray"/></a>
                     <a className="*:inline *:hover:fill-green-500" href="https://wa.me/+5584991095932" target="_blank"><FaWhatsapp color="gray"/></a>
