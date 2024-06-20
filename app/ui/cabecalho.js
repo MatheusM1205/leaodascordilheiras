ac5594f09615df069397e49264822900359d5e89
Matheus Medeiros <matheus.henrique.medeiros.ribeiro@outlook.com.br>
2024-06-20 11:28:56 -0300

    ...

--- app/ui/cabecalho.js
+++ app/ui/cabecalho.js
@@ -1,23 +1,23 @@
-import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaRegEnvelope } from "react-icons/fa";
+import { BsTelephone } from "react-icons/bs";
 
 
 export default function Cabecalho(){
     return (
         <div className="bg-neutral-900 py-1">
             <div className="container mx-auto flex">
-                <div className="*:inline space-x-4 basis-1/3">
-                    <FaFacebook color="gray"/>
-                    <FaInstagram color="gray"/>
-                    <FaYoutube color="gray"/>
+                <div className="space-x-4 basis-1/3">
+                    <a className="*:inline *:hover:fill-purple-500" href="https://www.instagram.com/maracatu.leao.das.cordilheiras/" target="_blank"><FaInstagram color="gray"/></a>
+                    <a className="*:inline *:hover:fill-red-500" href="https://www.youtube.com/@LeaodasCordilheiras" target="_blank"><FaYoutube color="gray"/></a>
                 </div>
                 <div className="basis-1/3"></div>
-                <div className="*:inline space-x-4 basis-1/3">
-                    <FaWhatsapp color="gray"/>
-                    <FaRegEnvelope color="gray"/>
+                <div className="space-x-4 basis-1/3 text-right">
+                    <a className="*:inline *:hover:fill-slate-100" href="tel:84991095932" target="_blank"><BsTelephone color="gray"/></a>
+                    <a className="*:inline *:hover:fill-green-500" href="https://wa.me/+5584991095932" target="_blank"><FaWhatsapp color="gray"/></a>
+                    <a className="*:inline *:hover:fill-slate-100" href="mailto:leao.das.cordilheiras.aracoiaba@gmail.com" target="_blank"><FaRegEnvelope color="gray"/></a>            
                 </div>
             </div>
         </div>
